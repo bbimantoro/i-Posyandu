be901aec7633cedffd680376b8a0ceb8ee26ab3d
alifa <febiyani_s1arsitektur@mahasiswa.ung.ac.id>
2023-08-11 17:49:04 +0800

    add hasil pemeriksaan, keamanan akun, lupa pass

--- app/src/main/java/app/dev/iposyandu/HasilPemeriksaanFragment.kt
+++ app/src/main/java/app/dev/iposyandu/HasilPemeriksaanFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [DataUser0Fragment.newInstance] factory method to
+ * Use the [HasilPemeriksaanFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class DataUser0Fragment : Fragment() {
+class HasilPemeriksaanFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class DataUser0Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data_user, container, false)
+        return inflater.inflate(R.layout.fragment_hasil_pemeriksaan, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class DataUser0Fragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment DataUser0Fragment.
+         * @return A new instance of fragment HasilPemeriksaanFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            DataUser0Fragment().apply {
+            HasilPemeriksaanFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/app/dev/iposyandu/KeamananAkunFragment.kt
+++ app/src/main/java/app/dev/iposyandu/KeamananAkunFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [DataUser0Fragment.newInstance] factory method to
+ * Use the [KeamananAkunFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class DataUser0Fragment : Fragment() {
+class KeamananAkunFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class DataUser0Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data_user, container, false)
+        return inflater.inflate(R.layout.fragment_keamanan_akun, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class DataUser0Fragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment DataUser0Fragment.
+         * @return A new instance of fragment KeamananAkunFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            DataUser0Fragment().apply {
+            KeamananAkunFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
